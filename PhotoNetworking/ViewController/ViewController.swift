b6d32a1347afe987169a226b2f54d8c714e6d457
caesar.rojas <caesar.rojas@gmail.com>
2022-11-08 03:48:02 -0500

    Last changes

--- PhotoNetworking/ViewController/ViewController.swift
+++ PhotoNetworking/ViewController/ViewController.swift
@@ -1,9 +1,4 @@
-//
-//  ViewController.swift
-//  IntroNetworking
-//
-//  Created by Consultant on 11/7/22.
-//
+
 
 import UIKit
 
@@ -19,34 +14,16 @@ class ViewController: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = .systemGreen
-        imageView.layer.cornerRadius = 20.0
+        imageView.layer.cornerRadius = 50.0
         return imageView
     }()
     
-//    lazy var stockButton: UIButton = {
-//        let button = UIButton(frame: .zero)
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.setTitle("Get Image", for: .normal)
-//        button.setTitleColor(.white, for: .normal)
-//        button.backgroundColor = .systemBlue
-//        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
-//        button.layer.cornerRadius = 20.0
-////        Register functionality
-        
-//        button.addTarget(self, action: #selector(self.stockButtonPressed(_:)), for: .touchUpInside)
-        
-//        return button
-//    }()
-    
     let network = NetworkManager()
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.setUpUI()
-//        self.createUI()
         
     }
     
@@ -58,10 +35,6 @@ class ViewController: UIViewController {
         table.translatesAutoresizingMaskIntoConstraints = false
         table.backgroundColor = .white
         table.dataSource = self
-//        table.delegate = self
-        
-        //        register the cell
-        //        table.register(UITableViewCell.self, forCellReuseIdentifier: "SampleCell")
         table.register(PhotoTableViewCell.self, forCellReuseIdentifier: "ProgCell")
         
         //        Add View to Hierarchy
@@ -71,45 +44,13 @@ class ViewController: UIViewController {
         table.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
         table.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
         table.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -8).isActive = true
+        
   
         self.tableView = table
     }
-//    func createUI(){
-//
-//        self.view.addSubview(self.stockImageView)
-//        self.view.addSubview(self.stockButton)
-//
-//        self.stockImageView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor, constant: -8).isActive = true
-//        self.stockImageView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
-//        self.stockImageView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
-        
-//        self.stockButton.topAnchor.constraint(equalTo: self.stockImageView.bottomAnchor, constant: 8).isActive = true
-//        self.stockButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 8).isActive = true
-//        self.stockButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -8).isActive = true
-//        self.stockButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -8).isActive = true
-        
 
-    }
+}
     
-//    @objc
-//    func stockButtonPressed(_ sender: UIButton){
-//        print("Button Pressed")
-
-//        let frame = self.stockImageView.frame
-//              self.network.fetchImageData(path: "https://picsum.photos/100/100") { [weak self] data in
-//
-////        self.network.fetchImageData(path: "https://picsum.photos/\(Int(frame.width))/\(Int(frame.height))") { [weak self] data in
-//            guard let data = data else { return }
-//            print(data)
-//            DispatchQueue.main.async {
-//                self?.stockImageView.image = UIImage(data: data)
-////                print(data)
-//            }
-//
-//        }
-//    }
-//}
-
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -124,39 +65,21 @@ extension ViewController: UITableViewDataSource {
         
         
         
-        let frame = stockImageView.frame
-        self.network.fetchImageData(path: "https://picsum.photos/100/100") { data in
+        _ = stockImageView.frame
+        self.network.fetchImageData(path: "https://picsum.photos/200/100") { data in
 
 //        self.network.fetchImageData(path: "https://picsum.photos/\(Int(frame.width))/\(Int(frame.height))") { [weak self] data in
             guard let data = data else { return }
             print(data)
             DispatchQueue.main.async {
-//                self?.stockImageView.image = UIImage(data: data)
-//                print(data)
+                print(data)
                 cell.ImageView.image = UIImage(data: data)
 
             }
         }
-      
-//        cell.textLabel?.text = "\(indexPath)"
-        
-//        cell.ImageView.image = UIImage(named: "1 gokultra")
-//        cell.ImageView.text = self.data[indexPath.row]
-//        cell.progMidLabel.text = self.dataInfo[indexPath.row]
-////        cell.progBotLabel.text = self.data[indexPath.row]
+    
         return cell
     }
     
 
 }
-
-
-//extension ViewController: UITableViewDelegate {
-//
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(indexPath)
-////        let placeholderVC = toysPlaceHolderViewController()
-////        self.navigationController?.pushViewController(placeholderVC, animated: true)
-//    }
-//
-//}
--- PhotoTableViewCell.swift
+++ PhotoTableViewCell.swift
@@ -1,9 +1,4 @@
-//
-//  PhotoTableViewCell.swift
-//  IntroNetworking
-//
-//  Created by Consultant on 11/7/22.
-//
+
 
 import UIKit
 
@@ -15,9 +10,20 @@ class PhotoTableViewCell: UITableViewCell {
         image.translatesAutoresizingMaskIntoConstraints = false
         image.backgroundColor = .systemBlue
         
-//        image.contentMode = .scaleAspectFit
+        image.contentMode = .scaleAspectFit
     
         return image
+    }()
+    
+    lazy var TopLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+//        label.backgroundColor = .systemCyan
+        label.numberOfLines = 0
+        label.text = "Random Image"
+        
+        return label
+        
     }()
     
     
@@ -30,14 +36,6 @@ class PhotoTableViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//
-////        setUPImg()
-//        // Initialization code
-//    }
 
 
     func setUPImg () {
@@ -45,25 +43,29 @@ class PhotoTableViewCell: UITableViewCell {
         self.backgroundColor = .white
         
         self.contentView.addSubview(self.ImageView)
+        self.contentView.addSubview(self.TopLabel)
         
         self.ImageView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 8).isActive = true
         self.ImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 8).isActive = true
         self.ImageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -8).isActive = true
-        self.ImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
-        self.ImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+//        self.ImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+//        self.ImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
         
         self.ImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        self.ImageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        self.ImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
 
+        self.TopLabel.leadingAnchor.constraint(equalTo: self.ImageView.trailingAnchor, constant: 10).isActive = true
+        self.TopLabel.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor, constant: 0).isActive = true
+
+
+        
         
     }
     
-    
-    
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
